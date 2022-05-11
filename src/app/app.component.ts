5d012af1a283f58435f7c844d4a88c1a28bbd24f
BrunoBatalha <brunobatalha37@gmail.com>
2022-05-10 23:02:57 -0400

    chore: config husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Lokin-FrontEnd';
+	title = 'Lokin-FrontEnd';
+
+	a(_: string): string {
+		return 'teste';
+	}
 }
