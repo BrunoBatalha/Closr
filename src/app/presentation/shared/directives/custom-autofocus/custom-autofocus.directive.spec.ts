cc2c0a80a474b5f3868f74225aa11cac5ab9c96a
BrunoBatalha <brunobatalha37@gmail.com>
2022-05-24 19:32:32 -0400

    fix: test directive custom autofocus

--- src/app/presentation/shared/directives/custom-autofocus/custom-autofocus.directive.spec.ts
+++ src/app/presentation/shared/directives/custom-autofocus/custom-autofocus.directive.spec.ts
@@ -2,7 +2,7 @@ import { CustomAutofocusDirective } from './custom-autofocus.directive';
 
 describe('CustomAutofocusDirective', () => {
 	it('should create an instance', () => {
-		const directive = new CustomAutofocusDirective();
+		const directive = new CustomAutofocusDirective({} as any);
 		expect(directive).toBeTruthy();
 	});
 });
