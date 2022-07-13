0c8ca2f1efa2f27a58fe3f0e2e8ed57140bc8930
BrunoBatalha <brunobatalha37@gmail.com>
2022-07-13 18:31:33 -0400

    feat: add errors

--- src/app/infra/constants/ErrorMessages.ts
+++ src/app/infra/constants/ErrorMessages.ts
@@ -1,3 +1,8 @@
 export const ERROR_MESSAGES = {
-	'ERR-CUE-0004': 'There is already a user with this email'
+	'ERR-CUE-0001': 'Email invalid',
+	'ERR-CUE-0002': 'Password invalid',
+	'ERR-CUE-0003': 'Username invalid',
+	'ERR-CUE-0004': 'There is already a user with this email',
+	'ERR-L-0001': 'Login invalid',
+	'ERR-L-0003': 'Login invalid'
 };
